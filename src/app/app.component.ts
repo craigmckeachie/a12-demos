ef8ef943170827219be7af29f642149d339bc93d
Craig McKeachie <craigdmckeachie@gmail.com>
2021-08-31 16:57:29 -0400

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` start `,
+  template: ` See console for output. `,
   styles: [],
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const observable$ = of(1, 2, 3);
+    observable$.subscribe((x) => console.log(x));
+  }
 }
