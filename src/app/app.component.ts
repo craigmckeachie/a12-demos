6e5bb200166e42fc1a2ad8474cf2205afd83117c
Craig McKeachie <craigdmckeachie@gmail.com>
2021-08-31 16:52:05 -0400

    add oninit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +6,6 @@ import { Component } from '@angular/core';
   template: ` start `,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {}
+}
