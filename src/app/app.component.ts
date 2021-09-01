cddf563fdd259a86bdbafcabc16ed53467bb7ce4
Craig McKeachie <craigdmckeachie@gmail.com>
2021-08-31 21:48:13 -0400

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { interval, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` start `,
+  template: ` See console for output. `,
   styles: [],
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const observable$ = interval(1000);
+    const observer: Observer<any> = {
+      next: (x) => console.log(x),
+      complete: () => console.log('completed'),
+      error: (x) => console.log(x),
+    };
+    const subscription = observable$.subscribe(observer);
+    setTimeout(() => subscription.unsubscribe(), 5000);
+  }
 }
