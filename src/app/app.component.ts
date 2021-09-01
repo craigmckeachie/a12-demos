cbe1630c809c2e7ca1d657ba38603f67d4f557ce
Craig McKeachie <craigdmckeachie@gmail.com>
2021-08-31 21:54:33 -0400

    strongly type creation operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { interval, Observer } from 'rxjs';
 export class AppComponent implements OnInit {
   ngOnInit(): void {
     const observable$ = interval(1000);
-    const observer: Observer<any> = {
+    const observer: Observer<number> = {
       next: (x) => console.log(x),
       complete: () => console.log('completed'),
       error: (x) => console.log(x),
