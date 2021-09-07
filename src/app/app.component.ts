f19886ff5db4308083e8dac66ab98a685cbc07fe
Craig McKeachie <craigdmckeachie@gmail.com>
2021-09-07 16:17:35 -0400

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` start `,
+  template: ` <app-fruit-list [fruits]="data"></app-fruit-list> `,
   styles: [],
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    const subject = new BehaviorSubject(123);
+  data = ['Apple', 'Orange', 'Plum'];
 
-    // two new subscribers will get initial value => output: 123, 123
-    subject.subscribe(console.log);
-    subject.subscribe(console.log);
-
-    // two subscribers will get new value => output: 456, 456
-    subject.next(456);
-
-    // new subscriber will get latest value (456) => output: 456
-    subject.subscribe(console.log);
-
-    // all three subscribers will get new value => output: 789, 789, 789
-    subject.next(789);
-  }
+  ngOnInit(): void {}
 }
