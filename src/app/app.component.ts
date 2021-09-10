0364418722ce11b0564abff0e870d60e95e7bb8f
Craig McKeachie <craigdmckeachie@gmail.com>
2021-09-09 20:32:27 -0400

    highlight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` start `,
+  template: `
+    <p appHighlight="yellow">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+      numquam repellat quibusdam sequi aperiam, culpa, obcaecati quam
+      praesentium ut soluta facere nulla corporis recusandae? Vitae perspiciatis
+      autem quibusdam unde voluptatem.
+    </p>
+  `,
   styles: [],
 })
 export class AppComponent implements OnInit {
