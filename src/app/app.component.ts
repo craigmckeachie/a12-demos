ecbac8782b9945b1d6dab2e33562a5b0541f0f72
Craig McKeachie <craigdmckeachie@gmail.com>
2022-02-21 18:50:41 -0500

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { PhotoService } from './photo.service';
   selector: 'app-root',
   template: `
     <h1>Photos</h1>
+    <p *ngIf="errorMessage">{{ errorMessage }}</p>
     <div *ngFor="let photo of photos">
       <img [src]="photo.thumbnailUrl" [alt]="photo.title" />
       <p>{{ photo.title }}</p>
@@ -16,8 +17,12 @@ import { PhotoService } from './photo.service';
 })
 export class AppComponent implements OnInit {
   photos: Photo[] = [];
+  errorMessage?: string | null;
   constructor(private photoService: PhotoService) {}
   ngOnInit(): void {
-    this.photoService.getAll().subscribe((data) => (this.photos = data));
+    this.photoService.getAll().subscribe(
+      (data) => (this.photos = data),
+      (error) => (this.errorMessage = error)
+    );
   }
 }
