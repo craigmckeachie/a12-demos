5d4775d575b182490da67169978b8e34adfac107
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-11 14:47:47 -0400

    reset start

--- src/app/ counter/shared/counter.actions.ts
+++
@@ -1,6 +0,0 @@
-// src\app\counter\shared\counter.actions.ts
-import { createAction } from "@ngrx/store";
-
-export const increment = createAction("[Counter] Increment");
-export const decrement = createAction("[Counter] Decrement");
-export const reset = createAction("[Counter] Reset");
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
